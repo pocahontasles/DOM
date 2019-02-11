cdcede116df2df7ed7ca943efdc399f196c57968
Olesia <45497738+pocahontasles@users.noreply.github.com>
2019-02-11 16:27:50 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+var playList = [
+    {
+        author: "LED ZEPPELIN",
+        song: "STAIRWAY TO HEAVEN"
+    },
+    {
+        author: "QUEEN",
+        song: "BOHEMIAN RHAPSODY"
+    },
+    {
+        author: "LYNYRD SKYNYRD",
+        song: "FREE BIRD"
+    },
+    {
+        author: "DEEP PURPLE",
+        song: "SMOKE ON THE WATER"
+    },
+    {
+        author: "JIMI HENDRIX",
+        song: "ALL ALONG THE WATCHTOWER"
+    },
+    {
+        author: "AC/DC",
+        song: "BACK IN BLACK"
+    },
+    {
+        author: "QUEEN",
+        song: "WE WILL ROCK YOU"
+    },
+    {
+        author: "METALLICA",
+        song: "ENTER SANDMAN"
+    }
+];
+
+var list = document.getElementById("list");
+for (var j = 0; j < playList.length; j++) {
+    var listItem = document.createElement("li");
+    listItem.innerText = playList[j].author + " - " + playList[j].song;
+    list.appendChild(listItem);
+}
